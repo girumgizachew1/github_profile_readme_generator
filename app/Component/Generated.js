e4cf4051b52489593082698774aada8629839a13
girum <girumegizachew@gmail.com>
2023-05-21 09:33:42 -0400

    basic structure

--- app/Component/Generated.js
+++ app/Component/Generated.js
@@ -1,16 +1,24 @@
 import React from 'react'
 
+import { GrView } from "react-icons/gr";
+import { BiCopy } from "react-icons/bi";
+import { AiOutlineDownload } from "react-icons/ai";
+import { FaFileCode } from "react-icons/fa";
+
+
+
+
 function Generated() {
   return (
     <div className='w-full h-full' >
-      <div className='flex space-x-2 mb-4 justify-between' >
-        <button className='border-2 border-zinc-800 px-6 py-1 flex justify-right' >Copy MarkDown</button>
-        <button className='border-2 border-zinc-800 px-6 py-1 flex justify-right' >Download MarkDown</button>
-        <button className='border-2 border-zinc-800 px-6 py-1 flex justify-right' >DownLoad Backup</button>
-        <button className='border-2 border-zinc-800 px-6 py-1 flex justify-right' >Preview</button>
+      <div className='flex space-x-2 mb-4 justify-between text-sm' >
+        <button className='border-2 bg-zinc-50 border-zinc-800 px-6 py-2 flex justify-right space-x-2'  ><BiCopy className='w-5 h-5' /> <h1>Copy MarkDown</h1></button>
+        <button className='border-2 bg-zinc-50 border-zinc-800 px-6 py-2 flex justify-right space-x-2'  ><AiOutlineDownload className='w-5 h-5' /> <h1>DownLoad MarkDown</h1></button>
+        <button className='border-2 bg-zinc-50 border-zinc-800 px-6 py-2 flex justify-right space-x-2'  ><FaFileCode className='w-5 h-5' /> <h1>DownLoad Backup</h1> </button>
+        <button className='border-2 bg-zinc-50 border-zinc-800 px-6 py-2 flex justify-right space-x-2'  ><GrView className='w-5 h-5' /> <h1>Preview</h1></button>
 
       </div>
-      <div className='border border-zinc-800 w-full h-full' >
+      <div className='border bg-zinc-50 border-zinc-800 w-full h-full' >
 
       </div>
     </div>
