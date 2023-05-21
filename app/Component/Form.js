59717a9a64b73387e0d107c771b75f92294d1818
girum <girumegizachew@gmail.com>
2023-05-21 08:33:22 -0400

    basic structure

--- app/Component/Form.js
+++ app/Component/Form.js
@@ -1,42 +1,44 @@
 import React, { useState } from 'react';
 
 function Form() {
-  const [title, setTitle] = useState('');
-  const [subtitle, setSubtitle] = useState('');
+    const [title, setTitle] = useState('');
+    const [subtitle, setSubtitle] = useState('');
 
-  const handleTitleChange = (event) => {
-    setTitle(event.target.value);
-  };
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value);
+    };
 
-  const handleSubtitleChange = (event) => {
-    setSubtitle(event.target.value);
-  };
+    const handleSubtitleChange = (event) => {
+        setSubtitle(event.target.value);
+    };
 
-  const generateReadme = () => {
-    // Logic to generate the README using the entered title and subtitle
-    console.log('Generating README...');
-  };
+    const generateReadme = () => {
+        // Logic to generate the README using the entered title and subtitle
+        console.log('Generating README...');
+    };
 
-  return (
-    <div className='space-y-10 justify-center mx-auto ' >
-      <div>
-        <label htmlFor="title">Title:</label>
-        <input
-          type="text"
-          id="title"
-          value={title}
-          onChange={handleTitleChange}
-        />
-        <input
-          type="text"
-          id="title"
-          value={title}
-          onChange={handleTitleChange}
-        />
-      </div>
-      <button onClick={generateReadme}>Generate README</button>
-    </div>
-  );
+    return (
+        <div className='space-y-10 justify-center p-2 w-full' >
+            <div>
+                <h1 className='text-xl' >Title</h1>
+                <div className='space-x-4' >
+                    <input
+                        type="text"
+                        id="title"
+                        value={title}
+                        onChange={handleTitleChange}
+                    />
+                    <input
+                        type="text"
+                        id="title"
+                        value={title}
+                        onChange={handleTitleChange}
+                    />
+                </div>
+            </div>
+            <button className='border border-zinc-800 px-6 py-2 flex justify-right' onClick={generateReadme}>Generate Read me</button>
+        </div>
+    );
 }
 
 export default Form;
