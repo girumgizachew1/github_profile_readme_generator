b07b91fd3764fc3867f703297461ce9eae39b7cc
girum <girumegizachew@gmail.com>
2023-05-23 09:22:27 -0400

    new font added

--- app/Component/Header.js
+++ app/Component/Header.js
@@ -4,16 +4,14 @@ import Image from 'next/image';
 function Header() {
   return (
     <header className="w-full h-28 flex flex-col ">
-      <div className='bg-zinc-100 flex w-full h-14 justify-between fixed ' >
+      <div className='bg-zinc-100 shadow-lg shadow-zinc-200 flex w-full h-14 justify-between fixed ' >
         <div className='flex space-x-4' >
           <div className="h-16 mt-2">
             <Image src="/logo2.png" alt="Logo" width={100} height={100} className='ml-10 w-10 h-10' />
           </div>
-          <h1 className="mr-10 text-xl text-zinc-800 font-bold text-center mt-4">GitHub Profile</h1>
+          <h1 className="mr-10 text-xl text-zinc-800  font-bold text-center mt-4">GitHub Profile</h1>
         </div>
-        <div>
-          <button>Darkmode</button>
-        </div>
+
       </div>
       <div className="flex justify-center mt-20 space-x-4">
         <button href="https://github.com/your-github-link" target="_blank" rel="noopener noreferrer" className="px-6 py-2 flex justify-right bg-cyan-500 text-white font-bold rounded-lg">
