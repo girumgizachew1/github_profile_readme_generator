9a442594a711d79a8a2f3fdc828ee55bafd767c8
girum <girumegizachew@gmail.com>
2023-05-23 14:39:00 -0400

    new font added

--- app/Component/Header.js
+++ app/Component/Header.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Image from 'next/image';
 
+import { AiOutlineStar } from 'react-icons/ai';
+import { BiGitRepoForked } from 'react-icons/bi';
+
+
 function Header() {
   return (
     <header className="w-full h-28 flex flex-col ">
@@ -14,11 +18,13 @@ function Header() {
 
       </div>
       <div className="flex justify-center mt-20 space-x-4">
-        <button href="https://github.com/your-github-link" target="_blank" rel="noopener noreferrer" className="px-6 py-2 flex justify-right bg-cyan-500 text-white font-bold rounded-lg">
-          Star this repo
+        <button href="https://github.com/your-github-link" target="_blank" rel="noopener noreferrer" className="space-x-2  px-6 py-2 flex justify-right bg-cyan-500 text-white font-bold rounded-lg">
+          <AiOutlineStar className='w-5 h-5 animate-spin'  />
+          <h1>Star this repo</h1>
         </button>
-        <button href="https://buymeacoffee.com/your-coffee-link" target="_blank" rel="noopener noreferrer" className="px-6 py-2 flex justify-right bg-yellow-500 text-white font-semibold rounded-lg">
-          Fork on Github
+        <button href="https://buymeacoffee.com/your-coffee-link" target="_blank" rel="noopener noreferrer" className="space-x-2 px-6 py-2 flex justify-right bg-yellow-500 text-white font-semibold rounded-lg">
+          <BiGitRepoForked className='w-5 h-5 animate-bounce' />
+          <h1>Fork on Github</h1>
         </button>
       </div>
     </header>
