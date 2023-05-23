5b9795ac6e152dbc643d3a0b5d85d4c2ce6dba0e
girum <girumegizachew@gmail.com>
2023-05-23 15:25:53 -0400

    new font added

--- app/Component/FormComponent/SocialSection.js
+++ app/Component/FormComponent/SocialSection.js
@@ -1,6 +1,11 @@
 import React from 'react'
+import { BsGithub, BsTwitter, BsStackOverflow,
+    BsLinkedin,
+    BsFacebook,
+    BsInstagram} from 'react-icons/bs';
+import { FaCodepen, FaDev, FaFacebook, FaInstagram, FaKaggle } from 'react-icons/fa';
+import { AiFillCodeSandboxCircle, AiOutlineCodepenCircle, AiOutlineCodeSandbox } from 'react-icons/ai';
 
-import { BsGithub, BsTwitter } from 'react-icons/bs';
 function SocialSection({
     github,
     handleGithubChange,
@@ -31,6 +36,86 @@ function SocialSection({
                         className="border border-zinc-300 focus:border-zinc-800 outline-none px-4 py-2 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none"
                     />
                 </div>
+                <div className='flex space-x-3 text-sm text-zinc-600'>
+                <BsLinkedin className='w-8 h-8' />
+                    <input
+                        type="text"
+                        id="title"
+                        value={twitter}
+                        onChange={handleTwitterChange}
+                        className="border border-zinc-300 focus:border-zinc-800 outline-none px-4 py-2 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none"
+                    />
+                </div>
+            </div>
+
+            <div className='space-x-3 flex'>
+                <div className='flex space-x-2 text-sm text-zinc-600'>
+                    <FaDev className='w-8 h-8' />
+                    <input
+                        type="text"
+                        id="title"
+                        value={github}
+                        onChange={handleGithubChange}
+                        className="border border-zinc-300 focus:border-zinc-800 outline-none px-4 py-2 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none"
+                    />
+                </div>
+                <div className='flex space-x-3 text-sm text-zinc-600'>
+                <FaCodepen className='w-8 h-8' />
+                    <input
+                        type="text"
+                        id="title"
+                        value={twitter}
+                        onChange={handleTwitterChange}
+                        className="border border-zinc-300 focus:border-zinc-800 outline-none px-4 py-2 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none"
+                    />
+                </div>
+
+                <div className='flex space-x-3 text-sm text-zinc-600'>
+                <FaFacebook className='w-8 h-8' />
+                    <input
+                        type="text"
+                        id="title"
+                        value={twitter}
+                        onChange={handleTwitterChange}
+                        className="border border-zinc-300 focus:border-zinc-800 outline-none px-4 py-2 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none"
+                    />
+                </div>
+            </div>
+
+
+
+            <div className='space-x-3 flex'>
+                <div className='flex space-x-2 text-sm text-zinc-600'>
+                    <AiFillCodeSandboxCircle className='w-8 h-8' />
+                    <input
+                        type="text"
+                        id="title"
+                        value={github}
+                        onChange={handleGithubChange}
+                        className="border border-zinc-300 focus:border-zinc-800 outline-none px-4 py-2 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none"
+                    />
+                </div>
+                <div className='flex space-x-2 text-sm text-zinc-600'>
+                    <BsStackOverflow className='w-8 h-8' />
+                    <input
+                        type="text"
+                        id="title"
+                        value={github}
+                        onChange={handleGithubChange}
+                        className="border border-zinc-300 focus:border-zinc-800 outline-none px-4 py-2 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none"
+                    />
+                </div>
+                <div className='flex space-x-2 text-sm text-zinc-600'>
+                    <FaInstagram className='w-8 h-8' />
+                    <input
+                        type="text"
+                        id="title"
+                        value={github}
+                        onChange={handleGithubChange}
+                        className="border border-zinc-300 focus:border-zinc-800 outline-none px-4 py-2 rounded-lg focus:ring-2 focus:ring-zinc-300 focus:outline-none"
+                    />
+                </div>
+                
             </div>
 
             
